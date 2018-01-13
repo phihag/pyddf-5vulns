ebbaa754baf81c3f64028f18a4a84e1d96ba2fdd
Philipp Hagemeister <phihag@phihag.de>
2018-01-13 21:16:22 +0100

    first version

---
+++ slides.js
@@ -0,0 +1,10 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+	Reveal.initialize({
+		transition: 'none',
+		slideNumber: 'c',
+		history: true,
+		center: false,
+	});
+});
