190d2a888e7a2a9a4a6bee11d04b32896e6edc30
Philipp Hagemeister <phihag@phihag.de>
2018-02-09 01:15:25 +0100

    switch to bottle

--- vulns.py
+++ vulns.py
@@ -13,67 +13,12 @@ import subprocess
 import sys
 import urllib.parse
 
-
-class VulnHandler(http.server.BaseHTTPRequestHandler):
-    views_by_method = {}
-
-    def send_text(self, txt, mime='text/html;charset=utf-8'):
-        bts = txt.encode('utf-8')
-        self.send_response(200)
-        self.send_header('Content-Type', mime)
-        self.send_header('Content-Length', '%s' % len(bts))
-        self.send_header(
-            'Cache-Control', 'no-cache, no-store, must-revalidate')
-        self.send_header('Pragma', 'no-cache')
-        self.send_header('Expires', '0')
-        self.end_headers()
-        self.wfile.write(bts)
-
-    def send_headers(self, headers):
-        for k, v in headers.items():
-            self.send_header(k, v)
-
-    def redirect(self, to, code=302, headers=None):
-        self.send_response(code)
-        self.send_header('Location', to)
-        self.send_header('Content-Type', 'text/plain')
-        if headers:
-            self.send_headers(headers)
-        self.end_headers()
-        self.wfile.write(('Redirect to %s' % to).encode('utf-8'))
-
-    def handle_parsed_request(self):
-        views = self.views_by_method.get(self.command, [])
-        for (rex, v) in views:
-            if rex.match(self.path):
-                return v(self)
-
-        self.send_error(404)
-
-    do_GET = handle_parsed_request
-    do_POST = handle_parsed_request
-    do_HEAD = handle_parsed_request
+import bottle
 
 
-def view(rex_str, method='GET'):
-    rex = re.compile(rex_str)
 
-    def decorate(func):
-        views = VulnHandler.views_by_method.setdefault(method, [])
-        views.append((rex, func))
-        return func
-
-    return decorate
-
-
-def _get_cookie(headers, key, default=None):
-    c = http.cookies.SimpleCookie(headers['Cookie'])
-    morsel = c.get(key)
-    return morsel.value if morsel else default
-
-
-def cur_user(handler):
-    session_b64 = _get_cookie(handler.headers, 'vulns_session')
+def _cur_user():
+    session_b64 = bottle.request.get_cookie('vulns_session')
     if not session_b64:
         return None
     session_json = base64.b64decode(session_b64)
@@ -84,23 +29,21 @@ def cur_user(handler):
     return user
 
 
-def parse_post(handler):
-    content_length = handler.headers['Content-Length']
-    if content_length is None:
-        return handler.send_error(411)
-    length = int(content_length)
+def require_user(func):
+    def handler():
+        user = _cur_user()
+        if not user:
+            bottle.abort(403, 'Please log in to user this function')
+            return
+        return func(user)
 
-    raw_body = handler.rfile.read(length)
-    tuples = urllib.parse.parse_qsl(raw_body)
-    dct = {k.decode('utf-8'): v.decode('utf-8') for k, v in tuples}
-    return dct
+    return handler
 
+@bottle.route('/')
+def root():
+    user = _cur_user()
 
-@view(r'^/$')
-def root_handler(handler):
-    user = cur_user(handler)
-
-    handler.send_text('''<!DOCTYPE html>
+    return ('''<!DOCTYPE html>
     <html>
     <head><meta charset="utf-8" />
     <style>
@@ -136,34 +79,35 @@ def root_handler(handler):
         </body></html>''')
 
 
-@view(r'^/login$', method='POST')
-def login(handler):
-    post = parse_post(handler)
-    assert post['user']
+@bottle.route('/login', method='POST')
+def login():
+    user = bottle.request.forms.get('user')
+    assert user
 
     session = {
-        'user': post['user'],
+        'user': user,
     }
     session_json = json.dumps(session)
     session_b64 = base64.b64encode(session_json.encode('utf-8'))
 
     expires = datetime.datetime.utcnow() + datetime.timedelta(days=400)
-    expires_str = expires.strftime("%a, %d %b %Y %H:%M:%S GMT")
-    handler.redirect('/', code=303, headers={
-        'Set-Cookie': 'vulns_session=' + session_b64.decode('utf-8') + '; expires=' + expires_str,
-    })
+    bottle.response.set_cookie('vulns_session', session_b64.decode('utf-8'), expires=expires)
+    bottle.redirect('/')
 
 
-@view(r'^/logout$', method='POST')
-def logout(handler):
-    handler.redirect('/', code=303, headers={
-        'Set-Cookie': 'vulns_session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT',
-    })
+@bottle.route('/logout', method='POST')
+def logout():
+    bottle.response.set_cookie('vulns_session', '', expires=0)
+    bottle.redirect('/')
 
 
-@view(r'^/transfer$', method='POST')
-def transfer(handler):
-    self.send_text('%s € wurde an %s überwiesen!' % (post['e'], post['to']))
+@bottle.route('/transfer')
+@require_user
+def transfer(user):
+    e = bottle.request.query['e']
+    to = bottle.request.query['to']
+    # TODO: Überweisungslogik hier
+    return '%s € wurde von %s an %s überwiesen!' % (e, user, to)
 
 
 def main():
@@ -189,8 +133,7 @@ def main():
 
 
 def run_server(args):
-    httpd = http.server.HTTPServer((args.addr, args.port), VulnHandler)
-    httpd.serve_forever()
+    bottle.run(host=args.addr, port=args.port, debug=True)
 
 
 def dev(args):
